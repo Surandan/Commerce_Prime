906e28d5fc603c905488bc095ea80b1a1852523a
Surandan <110124420+Surandan@users.noreply.github.com>
2023-07-26 17:34:11 +0530

    First Commit

---
+++ Prime-Commerce/src/main/java/com/example/PrimeCommerce/PrimeCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.PrimeCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeCommerceApplication.class, args);
+	}
+
+}
---
+++ Prime-Commerce/src/test/java/com/example/PrimeCommerce/PrimeCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PrimeCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
